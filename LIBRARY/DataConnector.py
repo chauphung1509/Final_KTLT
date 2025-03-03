153620b653cbf28385390f311bfcf05904f22beb
Phung Kim Chau <kimchauphung1509@gmail.com>
2025-03-03 18:51:49 +0700

    KC up

--- LIBRARY/DataConnector.py
+++ LIBRARY/DataConnector.py
@@ -1,17 +1,26 @@
-from LIBRARY.JsonFileFactory import JsonFileFactory
-from MODELS.Customer import Customer
+import pandas as pd
+
+from MODELS.Admin import Staff
 
 
 class DataConnector:
-    def get_all_customers(self):
-        jff = JsonFileFactory()
-        filename = "../dataset/employees.json" #chưa có
-        customers = jff.read_data(filename, Customer)
-        return customers
+    def get_all_staff(self):
+        filename = "../dataset/employees.xlsx"
+        df = pd.read_excel(filename)
 
-    def login(self,username,password):
-        employees=self.get_all_customers()
-        for e in employees:
-            if e.UserName==username and e.Password==password:
-                return e
-        return None
+        listofstaff = []
+        for _, row in df.iterrows():
+            staff = Staff(row['UserName'], row['Password'])
+            listofstaff.append(staff)
+        return listofstaff
+
+    def login(self, username, password):
+        listofstaff = self.get_all_staff()
+        if username in self.valid_accounts and self.valid_accounts[username] == password:
+            print("Đăng nhập thành công!")
+            return True
+        else:
+            print("Sai tên đăng nhập hoặc mật khẩu.")
+            return False
+
+#ê nha nó đang hơi lộn xộn
